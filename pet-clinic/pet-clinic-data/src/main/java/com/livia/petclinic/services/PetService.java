004fe03cb5922317a7f05a26ba523bddb3cf09a4
livia01px2019 <livia.zhu@live.com>
2020-11-20 14:48:58 +0800

    implement interfaces for services, closes #5

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/livia/petclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package com.livia.petclinic.services;
+
+import com.livia.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
---
+++ pet-clinic/pet-clinic-data/src/main/java/com/livia/petclinic/services/VetService.java
@@ -0,0 +1,15 @@
+package com.livia.petclinic.services;
+
+import com.livia.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+}
