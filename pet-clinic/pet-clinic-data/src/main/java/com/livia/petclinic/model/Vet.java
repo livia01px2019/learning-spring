b6992fd57bd6834cb525d51f8de96b48f454a7b7
livia01px2019 <livia.zhu@live.com>
2020-11-26 12:21:26 +0800

    specialty entity

--- pet-clinic/pet-clinic-data/src/main/java/com/livia/petclinic/model/Vet.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/livia/petclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package com.livia.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    Set<Specialty> specialities;
+
+    public Set<Specialty> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Specialty> specialities) {
+        this.specialities = specialities;
+    }
 }
