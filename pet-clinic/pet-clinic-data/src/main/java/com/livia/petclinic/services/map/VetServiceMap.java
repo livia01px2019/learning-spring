45ec49415da2eee04b934861dc140b4844b4b4fd
livia01px2019 <livia.zhu@live.com>
2020-11-26 13:46:22 +0800

    specialties bootstrap

--- pet-clinic/pet-clinic-data/src/main/java/com/livia/petclinic/services/map/VetServiceMap.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/livia/petclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.livia.petclinic.services.map;
 
+import com.livia.petclinic.model.Specialty;
 import com.livia.petclinic.model.Vet;
+import com.livia.petclinic.services.SpecialtyService;
 import com.livia.petclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,13 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -25,6 +34,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (object.getSpecialities().size() > 0) {
+            object.getSpecialities().forEach(specialty -> {
+               if(specialty.getId() == null) {
+                   Specialty savedSpecialty = specialtyService.save(specialty);
+                   specialty.setId(savedSpecialty.getId());
+               }
+            });
+        }
+
         return super.save(object);
     }
 
--- pet-clinic/pet-clinic-web/src/main/java/com/livia/petclinic/bootstrap/DataLoader.java
+++ pet-clinic/pet-clinic-web/src/main/java/com/livia/petclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package com.livia.petclinic.bootstrap;
 
-import com.livia.petclinic.model.Owner;
-import com.livia.petclinic.model.Pet;
-import com.livia.petclinic.model.PetType;
-import com.livia.petclinic.model.Vet;
+import com.livia.petclinic.model.*;
 import com.livia.petclinic.services.OwnerService;
 import com.livia.petclinic.services.PetTypeService;
+import com.livia.petclinic.services.SpecialtyService;
 import com.livia.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,16 +16,25 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialtyService specialtyService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialtyService = specialtyService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        int count = petTypeService.findAll().size();
 
+        if (count == 0) {
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -35,6 +42,14 @@ public class DataLoader implements CommandLineRunner {
         PetType cat = new PetType();
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
+
+        Specialty surgery = new Specialty();
+        surgery.setDescription("Surgery");
+        Specialty savedSurgery = specialtyService.save(surgery);
+
+        Specialty radiology = new Specialty();
+        radiology.setDescription("Radiology");
+        Specialty savedRadiology = specialtyService.save(radiology);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
@@ -73,12 +88,14 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Uel");
+        vet1.getSpecialities().add(savedSurgery);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jane");
         vet2.setLastName("Eyre");
+        vet2.getSpecialities().add(savedRadiology);
 
         vetService.save(vet2);
 
