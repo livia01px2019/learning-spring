affd832cd7d795fe515a042835da79e0c7ad0bf7
livia01px2019 <livia.zhu@live.com>
2020-11-26 12:28:55 +0800

    pettype service

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/livia/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.livia.petclinic.services;
+
+import com.livia.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
---
+++ pet-clinic/pet-clinic-data/src/main/java/com/livia/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,37 @@
+package com.livia.petclinic.services.map;
+
+import com.livia.petclinic.model.Pet;
+import com.livia.petclinic.model.PetType;
+import com.livia.petclinic.services.PetService;
+import com.livia.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
